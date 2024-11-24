a813bc6676576b8a192f7876792ae9006121e022
Hasanat902 <abulhasanat902@gmail.com>
2024-11-24 12:51:45 +0600

    connect with database and create user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+  console.log("Server is listening on port 3000");
 });
