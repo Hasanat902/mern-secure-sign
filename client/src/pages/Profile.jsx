de2fde62be2377d96c4fc0eb657a9d638c16d6b6
Hasanat902 <abulhasanat902@gmail.com>
2024-12-08 13:14:10 +0600

    added update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,48 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from "../redux/user/userSlice";
 
 const Profile = () => {
-  const { currentUser } = useSelector((state) => state.user);
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error));
+    }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <img
           src={currentUser.profilePicture}
           alt="profile"
@@ -18,6 +54,7 @@ const Profile = () => {
           id="username"
           placeholder="Username"
           className="bg-slate-200 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           defaultValue={currentUser.email}
@@ -25,15 +62,17 @@ const Profile = () => {
           id="email"
           placeholder="Email"
           className="bg-slate-200 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           id="password"
           placeholder="Password"
           className="bg-slate-200 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <button className="bg-slate-600 text-white p-3 uppercase rounded-lg font-semibold hover:opacity-95 disabled:opacity-80">
-          Update
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-4">
@@ -44,6 +83,10 @@ const Profile = () => {
           Sign Out
         </span>
       </div>
+      <p className="text-red-600 mt-5">{error && "Something went wrong!"}</p>
+      <p className="text-green-600 mt-5">
+        {updateSuccess && "User is updated successfully!"}
+      </p>
     </div>
   );
 };
